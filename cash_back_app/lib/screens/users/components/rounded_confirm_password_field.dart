59b734aab18d787cccf23905b0116e6a8e8e37c0
Yuyang Chen <chenyuyang0122@gmail.com>
2020-12-11 14:48:13 +0800

    12/11/2020 Start working on login and sign up pages

---
+++ cash_back_app/lib/screens/users/components/rounded_confirm_password_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import './text_field_container.dart';
+import '../../../constants.dart';
+
+class RoundedConfirmPasswordField extends StatelessWidget {
+  final ValueChanged<String> onChanged;
+  const RoundedConfirmPasswordField({
+    Key key,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        obscureText: true,
+        onChanged: onChanged,
+        cursorColor: kPrimaryColor,
+        decoration: InputDecoration(
+          hintText: "Confirm Password",
+          icon: Icon(
+            Icons.lock,
+            color: Colors.black,
+          ),
+          suffixIcon: Icon(
+            Icons.visibility,
+            color: Colors.black,
+          ),
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
--- cash_back_app/lib/screens/users/signup/component/body.dart
+++ cash_back_app/lib/screens/users/signup/component/body.dart
@@ -1,3 +1,11 @@
+import 'package:cash_back_app/screens/users/components/already_have_an_account_acheck.dart';
+import 'package:cash_back_app/screens/users/components/rounded_button.dart';
+import 'package:cash_back_app/screens/users/components/rounded_confirm_password_field.dart';
+import 'package:cash_back_app/screens/users/components/rounded_input_field.dart';
+import 'package:cash_back_app/screens/users/components/rounded_password_field.dart';
+import 'package:cash_back_app/screens/users/login/login.dart';
+import 'package:cash_back_app/screens/users/signup/component/or_divider.dart';
+import 'package:cash_back_app/screens/users/signup/component/social_icon.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
@@ -19,8 +27,55 @@ class Body extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
-                  "Sign up",
+                  "Sign Up",
                   style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: size.height * 0.03),
+                RoundedInputField(
+                  hintText: "Your Email",
+                  onChanged: (value) {},
+                ),
+                RoundedPasswordField(
+                  onChanged: (value) {},
+                ),
+                RoundedConfirmPasswordField(
+                  onChanged: (value) {},
+                ),
+                RoundedButton(
+                  text: 'Sign up',
+                  press: () {},
+                ),
+                SizedBox(height: size.height * 0.03),
+                AlreadyHaveAnAccountCheck(
+                  login: false,
+                  press: () {
+                    Navigator.push(
+                      context, 
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginScreen();
+                        }
+                      )
+                    );
+                  },
+                ),
+                OrDivider(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SocalIcon(
+                      iconSrc: "assets/icons/facebook.svg",
+                      press: () {},
+                    ),
+                    SocalIcon(
+                      iconSrc: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                    SocalIcon(
+                      iconSrc: "assets/icons/google-plus.svg",
+                      press: () {},
+                    ),
+                  ],
                 )
               ],
             ),
